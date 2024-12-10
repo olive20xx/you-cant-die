9387334d3fdcb8a5bc7b87dd60ff23593ff24563
Craig Ostrin <craigostrin@gmail.com>
2024-12-09 20:44:51 -0800

    movement input settings

--- src/player.gd
+++ src/player.gd
@@ -6,8 +6,8 @@ const SPEED = 300.0
 
 func _physics_process(_delta: float) -> void:
 	var direction := Vector2.ZERO 
-	direction.x = Input.get_axis("ui_left", "ui_right")
-	direction.y = Input.get_axis("ui_up", "ui_down")
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
 	
 	if direction:
 		velocity = direction * SPEED
