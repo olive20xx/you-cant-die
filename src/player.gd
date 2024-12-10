936b89f6d4fe4a0610c833af9321d496527d9214
Craig Ostrin <craigostrin@gmail.com>
2024-12-09 21:02:17 -0800

    healing

--- src/player.gd
+++ src/player.gd
@@ -1,10 +1,50 @@
+class_name Player
 extends CharacterBody2D
 
+# Healing should do something cool/fun/stylish if you have no shields!
 
+const HEAL_TIME = 1.5
 const SPEED = 300.0
+
+var can_move := true
+
+@onready var shields: Shields = $Shields
+@onready var heal_timer: Timer = $HealTimer
+@onready var heal_timer_label: Label = $HealTimerLabel #debug
+
+
+func _ready() -> void:
+	heal_timer.wait_time = HEAL_TIME
+	heal_timer.timeout.connect(_heal)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("heal"):
+		start_healing()
+	
+	if event.is_action_released("heal"):
+		can_move = true
+		heal_timer.stop()
 
 
 func _physics_process(_delta: float) -> void:
+	debug_update_heal_timer_label()
+	
+	if can_move:
+		move()
+
+
+func start_healing() -> void:
+	can_move = false
+	heal_timer.start()
+
+
+func _heal() -> void:
+	shields.disable_random()
+	can_move = true
+
+
+func move() -> void:
 	var direction := Vector2.ZERO 
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
@@ -15,3 +55,7 @@ func _physics_process(_delta: float) -> void:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 
 	move_and_slide()
+
+
+func debug_update_heal_timer_label() -> void:
+	heal_timer_label.text = "%.1f" % heal_timer.time_left
--- src/shields.gd
+++ src/shields.gd
@@ -8,12 +8,22 @@ enum Shield {
 	BOT_RIGHT
 }
 
-@onready var top_left: Area2D = $TopLeft
-@onready var top_right: Area2D = $TopRight
-@onready var bot_left: Area2D = $BotLeft
-@onready var bot_right: Area2D = $BotRight
+@onready var top_left: CharacterBody2D = $TopLeft
+@onready var top_right: CharacterBody2D = $TopRight
+@onready var bot_left: CharacterBody2D = $BotLeft
+@onready var bot_right: CharacterBody2D = $BotRight
 
-@onready var shields: Array[Area2D] = [top_left, top_right, bot_left, bot_right]
+@onready var shields: Array[CharacterBody2D] = [top_left, top_right, bot_left, bot_right]
+
+
+func enable_random() -> void:
+	var i := randi_range(0, 3)
+	enable(i)
+
+
+func disable_random() -> void:
+	var i := randi_range(0, 3)
+	disable(i)
 
 
 func enable(index: Shield) -> void:
